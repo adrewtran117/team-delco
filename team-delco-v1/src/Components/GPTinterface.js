eea9ad8d9a2b1d99ce3546a320b8e2b126709c2d
adrewtran117 <adrewtran117@gmail.com>
2022-07-23 12:26:21 -0400

    add GPT3 to spatial

---
+++ team-delco-v1/src/Components/GPTinterface.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import GenerateText from './ButtonTypes/Gpt3';
+
+function GPTInterface() {
+  return (
+    <div style={{}}>
+        <div>
+            <GenerateText/>
+        </div>
+    </div>
+  );
+}
+
+export default GPTInterface;
--- team-delco-v1/src/Pages/Exploration.js
+++ team-delco-v1/src/Pages/Exploration.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import SpatialTitle from '../Components/SpatialTitle';
 import SpatialMenu from '../Components/SpatialExploration';
+import GPT3Interface from '../Components/GPTinterface';
 
 function SpatialPage() {
   const mystyle = {
@@ -11,6 +12,7 @@ function SpatialPage() {
     <div style={mystyle} className="App">
         <SpatialTitle/>
         <SpatialMenu/>
+        <GPT3Interface/>
     </div>
   );
 }
