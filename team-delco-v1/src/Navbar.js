3418d7355a7ba57f73a21e0be6abe76899f3fefa
Chiku Okechukwu <chikuokechukwu@Chikus-Air.fios-router.home>
2022-07-27 15:45:58 -0400

    new colors and whatnot

--- team-delco-v1/src/Navbar.js
+++ team-delco-v1/src/Navbar.js
@@ -1,28 +1,33 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
-
+import logo from "./Components/HumanityWithUS.png"
 export default function Navbar() {
-  return (
-    <nav className="nav">
-      <Link to="/" className="site-title">
-        Poverty Watch
-      </Link>
-      <ul>
-        <CustomLink to="/about">About</CustomLink>
-        <CustomLink to="/spatial">Spatial</CustomLink>
-      </ul>
-    </nav>
-  )
+    return ( <
+        nav className = "nav" >
+        <
+        Link to = "/"
+        className = "site-title" >
+        <
+        img src = { logo }
+        / > < /
+        Link > <
+        ul >
+        <
+        CustomLink to = "/about" > About < /CustomLink> <
+        CustomLink to = "/spatial" > Spatial < /CustomLink> < /
+        ul > <
+        /nav>
+    )
 }
 
 function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
 
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  )
+    return ( <
+        li className = { isActive ? "active" : "" } >
+        <
+        Link to = { to } {...props } > { children } <
+        /Link> < /
+        li >
+    )
 }
